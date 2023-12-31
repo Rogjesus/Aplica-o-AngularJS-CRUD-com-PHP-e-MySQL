d1803b8b74866ddd3fe211a221c2791a4a173a60
Rogerio Silva <rogeriojesus906@gmail.com>
2023-12-31 11:27:37 -0300

    abc.php

---
+++ abc.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body>
+<form method="post" action="bb.php">
+<input type="text" name="t1">
+<input type="submit">
+</form>
+</body>
+</html>
